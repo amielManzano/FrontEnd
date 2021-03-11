6bfee4ddbf60be44f4fa5a24af6280e96b8a1808
aemkoala <73735088+aemkoala@users.noreply.github.com>
2021-03-11 21:52:37 +0800

    catalog page api

--- pages/catalog.js
+++ pages/catalog.js
@@ -1,11 +1,12 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
 import React, {useState, useEffect} from 'react'
-import { Form } from 'react-bootstrap'
+import { Form, Card } from 'react-bootstrap'
 
 export default function Home() {
     const [categoryName, setCategoryName] = useState("")
     const [selectedCategory, setSelectedCategory] = useState("")
+    const [products, setProducts] = useState("")
 
     useEffect(() => {
         let urlAll = 'https://magic-aliexpress1.p.rapidapi.com/api/v2/categories'
@@ -60,6 +61,28 @@ export default function Home() {
                                 .then(response => response.json())
                                 .then(data => {
                                     console.log(data)
+                                    let allProducts = data.docs.map(i => {
+                                        return(
+                                            <div className='col-4'>
+                                            <Card style={{ width: '18rem' }} className='my-3'>
+                                            <Card.Img variant="top" src={i.product_main_image_url} />
+                                            <Card.Body>
+                                                <Card.Title>{i.product_title}</Card.Title>
+                                                <Card.Text>
+                                                    Price: {i.app_sale_price_currency} {i.app_sale_price}
+                                                </Card.Text>
+                                            </Card.Body>
+                                            <Card.Footer>
+                                                <>
+                                               <a href={i.product_detail_url} className='btn btn-secondary w-100 mb-2'>View Details</a>
+                                               <a href='#' className='btn btn-secondary w-100'>Checkout</a>
+                                               </>
+                                            </Card.Footer>
+                                            </Card>
+                                            </div>
+                                        )
+                                    })
+                                    setProducts(allProducts)
                                 })
                         }
                     
@@ -73,6 +96,7 @@ export default function Home() {
    
   return (
     <>
+        <div className='container'>
         <h1 className='text-center mt-5'>Catalog</h1>
         <Form.Group controlId="categoryName">
             <Form.Label className='colorWhite'>Category name:</Form.Label>
@@ -82,6 +106,12 @@ export default function Home() {
                         {categoryName}
             </Form.Control>
         </Form.Group>
+        
+            <div className='row'>
+                {products}
+            </div>
+        </div>
+        
     </>
   )
 }
