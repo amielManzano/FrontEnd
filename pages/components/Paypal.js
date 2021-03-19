8abcfe7678d2e348e0904d15797bf9b47094ee03
amielManzano <manzano.amiel.e@gmail.com>
2021-03-19 10:56:07 +0800

    deploy

--- pages/components/Paypal.js
+++ pages/components/Paypal.js
@@ -7,6 +7,7 @@ import { useRouter } from 'next/router'
 const PaypalBtn = ()=>{
     const router = useRouter()
     const[totalPurchase, setTotalPurchase] = useState('')
+    const[allOrders, setAllOrders] = useState('')
     let totalValueFromCart=0
     // console.log(localStorage.getItem('totalToPay'))
         const refPaypalBtn = useRef()
@@ -30,20 +31,63 @@ const PaypalBtn = ()=>{
            console.log(totalValueFromCart)
            localStorage.setItem('totalToPay', totalValueFromCart)
 
+           
         })
 
         useEffect(()=>{
            paypal.Buttons({
                 createOrder: function(data, actions) {
                   // This function sets up the details of the transaction, including the amount and line item details.
-                  return actions.order.create({
-                    description: 'Fish',
-                    purchase_units: [{
-                      amount: {
-                        value: localStorage.getItem('totalToPay')
-                      }
-                    }]
-                  });
+                    // console.log(`from state: ${allOrders  }`)
+                    fetch('https://mighty-garden-47499.herokuapp.com/user-details', {
+                      method: 'POST',
+                      headers: { 'CONTENT-TYPE': 'APPLICATION/JSON'},
+                      body: JSON.stringify({
+                        userId: localStorage.getItem('userId')
+                      })
+                    })
+                    .then(response => response.json())
+                    .then(data => {
+                      data.cart.map( i => {
+                        // console.log(i)
+                        fetch('https://mighty-garden-47499.herokuapp.com/checkout-product', {
+                          method: 'POST',
+                          headers: { 'CONTENT-TYPE': 'APPLICATION/JSON'},
+                          body: JSON.stringify({
+                            userId: localStorage.getItem('userId'),
+                            productId: i.productId,
+                            product_title: i.product_title,
+                            app_sale_price: i.app_sale_price,
+                            product_main_image_url: i.product_main_image_url
+                          })
+                        })
+                        .then(response => response.json())
+                        .then(data => {
+                            console.log('pushed')
+                        })
+                     
+                        
+                      })
+                     
+                        
+                    })
+        
+
+                    let finalData = localStorage.getItem('allOrdersHere')
+                    console.log(`finalData ${finalData}` )
+                    if(finalData){
+                      return actions.order.create({
+                        purchase_units: [{
+                          description: "All orders",
+                          amount: {
+                            value: localStorage.getItem('totalToPay')
+                          },
+                          // note_to_payer: 'Contact us for any questions on your order.'
+
+                        }]
+                      });
+                    }
+                 
                 },
                 onApprove: function(data, actions) {
                   // This function captures the funds from the transaction.
@@ -51,6 +95,11 @@ const PaypalBtn = ()=>{
                     // This function shows a transaction success message to your buyer.
                 
                     // alert('Transaction completed');
+                   
+                    // push to purchased order
+                  
+                    
+                    
 
                     fetch('https://mighty-garden-47499.herokuapp.com/paypal', {
                       method: 'POST',
@@ -67,7 +116,7 @@ const PaypalBtn = ()=>{
                           icon: "success",
                           button: "continue",
                         });
-                        window.location.replace('/checkout')
+                        //window.location.replace('/checkout')
                       })
                     })
                     
